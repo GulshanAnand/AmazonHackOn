214d2d14e5a8f524c8bc8955debc4cc048f171a6
Gulshan Anand <gulshan2052@gmail.com>
2023-10-24 18:34:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT||3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const OpenAI = require('openai');
+
+const openai = new OpenAI({
+  apiKey: process.env.apikey,
+});
+
+app.post('/api', async (req, res) => {
+    console.log(req.body.prompt);
+
+    try {
+        const response = await openai.chat.completions.create({
+          messages: [
+            { role: 'system', content: req.body.prompt }
+          ],
+          model: 'gpt-3.5-turbo',
+        });
+        
+        const messageBot = response.data.choices[0].message.content;
+        console.log(messageBot);
+
+        res.json({msg:'ok', query: messageBot});
+      } catch (error) {
+        console.error('Error:', error);
+        res.json({msg:'not ok', query: 'Something went wrong'});
+      }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
